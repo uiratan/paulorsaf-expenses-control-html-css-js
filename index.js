418be42b2a73adf568b328c230be47c09c1fa83a
uiratan <uiratan@gmail.com>
2023-03-03 17:54:34 -0300

    Mensagens de erro no formulário de Login com Javascript

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function validateFields() {
-  const emailValid = isEmailValid();
-  document.getElementById("recover-password-button").disabled = !emailValid;
+function onChanceEmail() {
+  toggleEmailErrors();
+  toggleButtonsDisabled();
+}
 
-  const passwordValid = isPasswordValid();
-  document.getElementById("login-button").disabled = !emailValid || !passwordValid;
-
+function onChancePassword() {
+  togglePasswordErrors();
+  toggleButtonsDisabled();
 }
 
 function isEmailValid() {
@@ -15,6 +16,42 @@ function isEmailValid() {
   return validateEmail(email);
 }
 
+function toggleEmailErrors() {
+  const email = document.getElementById("email").value;
+
+  if (!email) {
+    document.getElementById("email-required-error").style.display="block";
+  } else {
+    document.getElementById("email-required-error").style.display="none";
+  }
+
+  if (validateEmail(email)) {
+    document.getElementById("email-invalid-error").style.display="none";
+  } else {
+    document.getElementById("email-invalid-error").style.display="block";
+  }
+
+}
+
+function togglePasswordErrors() {
+  const password = document.getElementById("password").value;
+
+  if (!password) {
+    document.getElementById("password-required-error").style.display="block";
+  } else {
+    document.getElementById("password-required-error").style.display="none";
+  }
+
+}
+
+function toggleButtonsDisabled() {
+  const emailValid = isEmailValid();
+  document.getElementById("recover-password-button").disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+}
+
 function isPasswordValid() {
   const password = document.getElementById("password").value;
   if (!password) {
@@ -22,6 +59,7 @@ function isPasswordValid() {
   }
   return true;
 }
+
 
 function validateEmail(email) {
   return /\S+@\S+\.\S+/.test(email);
