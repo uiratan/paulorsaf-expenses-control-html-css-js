3dcffba534f3cac67bd9fb54ed5af5099a57f16f
uiratan <uiratan@gmail.com>
2023-03-04 15:29:06 -0300

    Recuperação de senha com Firebase Authentication

--- index.js
+++ index.js
@@ -24,12 +24,28 @@ function getErrorMessage(error) {
   if (error.code == 'auth/user-not-found') {
     return 'Usuário não encontrado';
   }
+
+  if (error.code == 'auth/wrong-password') {
+    return 'Senha inválida';
+  }
+
   return error.message;
 }
 
 function register() {
+  window.location.href = 'pages/register/register.html'
+}
+
+function recoverPassword() {
   showLoading();
-  //window.location.href = 'pages/register/register.html'
+  firebase.auth().sendPasswordResetEmail(form.email().value)
+    .then(() => {
+      hideLoading();
+      alert('Email enviado com sucesso.');
+    }).catch(error => {
+      hideLoading();
+      alert(getErrorMessage(error));
+    })
 }
 
 function isEmailValid() {
